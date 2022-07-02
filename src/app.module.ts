f8953567d93cddc7fd88179ef7128345d0f18cac
bilard <bilardosinulu@gmail.com>
2022-07-02 11:54:32 +0100

    Created an Entity and Repository Setup Body Validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { UserEntity } from './users/users.entity';
+import { ReportEntity } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [UserEntity, ReportEntity],
       synchronize: true,
     }),
     UsersModule,
